f705133f6c16b193d3b5e3c840add99e55f5c9fd
Thiago Oliveira <thiagomodev@gmail.com>
2022-10-26 14:07:45 -0300

    add resize grid function

--- script.js
+++ script.js
@@ -7,31 +7,53 @@ function createDiv() {
 }
 
 
-function hover() {
+function hover(firstDiv) {
     const item = document.querySelectorAll('.item');
     item.forEach(item => {
         item.addEventListener('mouseover', e => {
-            if(container.classList.contains('active')){
+            if (container.classList.contains('active')) {
                 item.style.backgroundColor = 'black';
+                firstDiv.style.backgroundColor = 'black';
+                console.log(e)
             }
         });
     });
+
 }
 
+function makeGrid(size){
+    const displaySize = 600;
+    let gridSize = [];
+    let divSize = displaySize / size;
+    for(let i = 0; i < size; i++){
+        gridSize.push(divSize);
+    }
+    const gridStr = gridSize.toString().split(',').join('px ') + 'px';
 
+    container.style.gridTemplateColumns = gridStr;
+    container.style.gridTemplateRows = gridStr;
 
-for (let i = 0; i < 16 * 16; i++) {
-    const div = createDiv();
-    container.appendChild(div);
+    for (let i = 0; i < size * size; i++) {
+        const div = createDiv();
+        container.appendChild(div);
+    }
+
 }
+
+makeGrid(64);
 
 container.addEventListener('mousedown', e => {
     container.classList.add('active');
-    hover();
+    hover(e.target);
 });
 
-container.addEventListener('mouseup', e => {
-    container.classList.remove('active');
-})
+
+
+['mouseup', 'mouseleave'].forEach(evt => {
+    container.addEventListener(evt, () => {
+        container.classList.remove('active');
+    });
+});
+
 
 
